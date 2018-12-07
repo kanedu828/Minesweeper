75408182a21dadc87165a635c052cd82d5a4630f
Kane Du <45084706+kanedu828@users.noreply.github.com>
2018-12-07 04:03:01 -0500

    Update PlayMinesweeper.java

--- PlayMinesweeper.java
+++ PlayMinesweeper.java
@@ -42,6 +42,7 @@ public class PlayMinesweeper{
             map.mapToString();
             if(map.win() == true){
                 System.out.println("MAP COMPLETED, YOU WIN!");
+                System.exit(0);
             }
         }
     }
